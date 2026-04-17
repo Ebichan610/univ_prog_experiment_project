a39e29e8b765e7749de15a9b6a37f542104d5894
Ebichan610 <yuya.ebi@gmail.com>
2026-04-17 13:46:19 +0900

    new:test files, add:1&2 content

--- cpj2026_2412747_2/2412747_kadai2_1.cpp
+++ cpj2026_2412747_2/2412747_kadai2_1.cpp
@@ -1,9 +1,67 @@
 #include <iostream>
-#include <fstream>
-#include <cerrono>
+#include <cstdio>
 using namespace std;
 
-int main()
+//最大公約数を求める関数
+static int calc_gcd(int m, int n)
 {
+    int r = m % n;
+    while(r != 0)
+    {
+        m = n;
+        n = r;
+        r = m % n;
+    }
+    return(n);
+}
 
+int main(int argc, char *argv[])
+{
+    //引数の数のエラー処理
+    if(argc != 3)
+    {
+        cerr << "引数の数が2つではありません。入力し直してください。\n";
+        return(1);
+    }
+    //引数が数字でない
+    for(int i = 0; argv[1][i] != '\0'; i++)
+    {
+        if(argv[1][i] < '0' || argv[1][i] > '9')
+        {
+            cerr << "引数に0~9の数字以外の文字が入っています。正の整数を入力してください。\n";
+            return(1);
+        }
+    }
+    for(int j = 0; argv[1][j] != '\0'; j++)
+    {
+        if(argv[1][j] < '0' || argv[1][j] > '9')
+        {
+            cerr << "引数に0~9の数字以外の文字が入っています。正の整数を入力してください。\n";
+            return(1);
+        }
+    }
+    //引数の数字を格納
+    int x = atoi(argv[1]), y = atoi(argv[2]);
+    int m, n;
+    //数の大小によってmとnに代入する数字を変える
+    if(x >= y)
+    {
+        m = x;
+        n = y;
+    }
+    else
+    {
+        m = y;
+        n = x;
+    }
+    //0は弾く
+    if(m == 0 || n == 0)
+    {
+        cerr << "正の整数を入力してください。\n";
+        return(1);
+    }
+    //最大公約数を求める処理
+    int gcd = calc_gcd(m, n);
+    cout << argv[1] << "と" << argv[2] << "の最大公約数は" << gcd << "です。\n";
+    return(0);
 }
---
+++ cpj2026_2412747_2/2412747_kadai2_2.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <fstream>
+#include <cerrno>
+using namespace std;
+
+template <class T>
+T sumt(T x, T y)
+{
+    return (x + y);
+}
+
+int main(int argc, char *argv[])
+{
+    //引数の数のエラー処理
+    if(argc != 3)
+    {
+        cerr << "引数の数が2つではありません。\n";
+        return(1);
+    }
+    //ファイルの読み取りおよびエラー処理
+    ifstream fin1(argv[1]);
+    if(!fin1)
+    {
+        cerr << "1つ目のファイルを開けませんでした。\n";
+        return(errno);
+    }
+    ifstream fin2(argv[2]);
+    if(!fin2)
+    {
+        cerr << "2つ目のファイルを開けませんでした。\n";
+        return(errno);
+    }
+    //ファイルの行数の変数を宣言
+    int fin1_len = 0, fin2_len = 0;
+    //ファイル1と2それぞれの合計の変数と数字を一時的に保存する変数の宣言
+    int fin1_sum = 0, fin1_tmp = 0;
+    double fin2_sum = 0.0, fin2_tmp = 0.0;
+    //ループ処理で合計をそれぞれ計算
+    while(fin1 >> fin1_tmp)
+    {
+        fin1_sum = sumt(fin1_tmp, fin1_sum);
+        fin1_len++;
+    }
+    while(fin2 >> fin2_tmp)
+    {
+        fin2_sum = sumt(fin2_tmp, fin2_sum);
+        fin2_len++;
+    }
+    //出力
+    cout << "1つ目のファイルは" << fin1_len << "行で、記載されている数字の合計は" << fin1_sum << "です\n";
+    cout << "2つ目のファイルは" << fin2_len << "行で、記載されている数字の合計は" << fin2_sum << "です\n";
+    return(0);
+}
