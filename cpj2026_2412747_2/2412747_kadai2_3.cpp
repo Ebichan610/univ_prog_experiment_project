219041d996363d08f608091153f54a02aeac64cf
Ebichan610 <yuya.ebi@gmail.com>
2026-04-17 16:39:45 +0900

    fin:3

--- cpj2026_2412747_2/2412747_kadai2_3.cpp
+++ cpj2026_2412747_2/2412747_kadai2_3.cpp
@@ -4,10 +4,14 @@ using namespace std;
 //経路数を計算するプログラム
 int calc_route(int x, int y)
 {
-    //始点を宣言
-    int route = 0;
-    if(x == 0 && y == 0)
-        return(route);
+    //例外処理
+    if(x == 4 && y == 2)
+        return(0);
+    //終了条件
+    if(x == 0 || y == 0)
+        return(1);
+    //再帰的に計算
+    return(calc_route(x - 1, y) + calc_route(x, y - 1));
 }
 
 int main(int argc, char *argv[])
@@ -27,9 +31,9 @@ int main(int argc, char *argv[])
             return(1);
         }
     }
-    for(int j = 0; argv[1][j] != '\0'; j++)
+    for(int j = 0; argv[2][j] != '\0'; j++)
     {
-        if(argv[1][j] < '0' || argv[1][j] > '9')
+        if(argv[2][j] < '0' || argv[2][j] > '9')
         {
             cerr << "引数に0~9の数字以外の文字が入っています。正の整数を入力してください。\n";
             return(1);
@@ -38,6 +42,12 @@ int main(int argc, char *argv[])
     //引数の数字を変数に格納
     int x = atoi(argv[1]);
     int y = atoi(argv[2]);
+    //入力が(4,2)のときをエラー扱いに
+    if(x == 4 && y == 2)
+    {
+        cerr << "(4,2)は工事中なため使用できません。\n";
+        return(1);
+    }
     //経路数を計算
     int route = calc_route(x, y);
     cout << "終点座標が(" << x << "," << y <<")のときの経路数は" << route << "です。\n"; 
