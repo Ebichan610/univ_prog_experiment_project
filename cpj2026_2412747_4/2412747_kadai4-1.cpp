0682ce6e1ca920deda73bfb94dca246a35a129cd
Ebichan610 <yuya.ebi@gmail.com>
2026-05-01 14:39:56 +0900

    a

--- cpj2026_2412747_4/2412747_kadai4-1.cpp
+++ cpj2026_2412747_4/2412747_kadai4-1.cpp
@@ -1,16 +1,78 @@
 #include <iostream>
+#include <cstdio>
+#include <cctype>
 using namespace std;
 
+typedef struct s_comp_num
+{
+    double real;
+    double img;
+}   t_comp_num;
+
+//二次方程式の各定数をもつ構造体
 typedef struct s_quad
 {
     double a;
     double b;
     double c;
+    t_comp_num ans1;
+    t_comp_num ans2;
 }   t_quad;
 
+//引数チェック用関数
 static bool check_arg(char* arg)
 {
+    while(isspace(*arg))
+        arg++;
+    while(*arg == '+' || *arg == '-')
+        arg++;
+    if(*arg < '0' || *arg > '9')
+        return(false);
+    while(*arg >= '0' && *arg <= '9')
+        arg++;
+    if(*arg != '\0')
+        return(false);
+    return(true);
+}
 
+//引数の数値をquad構造体のメンバに代入して初期化する関数
+static t_quad initialize_quad(char* argv[])
+{
+    t_quad quad;
+
+    quad.a = atoi(argv[1]);
+    quad.b = atoi(argv[2]);
+    quad.c = atoi(argv[3]);
+    return(quad);
+}
+
+static void calc_quad(t_quad& quad)
+{
+    //計算結果を保存する変数
+    t_comp_num res1, res2;
+    //複素数解とならない場合
+    if(quad.b * quad.b - 4 * quad.a * quad.c >= 0)
+    {
+        //虚数部は存在せず
+        res1.img = 0;
+        res2.img = 0;
+        //実数部の計算
+        res1.real = (-quad.b - sqrt(quad.b * quad.b - 4 * quad.a * quad.c)) / 2.0 * quad.a;
+        res2.real = (-quad.b + sqrt(quad.b * quad.b - 4 * quad.a * quad.c)) / 2.0 * quad.a;
+    }
+    //複素数解となる場合
+    else
+    {
+        //虚数部単体の計算
+        res1.img = -sqrt(4 * quad.a * quad.c - quad.b * quad.b) / 2.0 * quad.a;
+        res2.img = sqrt(4 * quad.a * quad.c - quad.b * quad.b) / 2.0 * quad.a;
+        //実数部単体の計算
+        res1.real = -quad.b;
+        res2.real = -quad.b;
+    }
+    //アドレス引数のquadにそれぞれの解を代入
+    quad.ans1 = res1;
+    quad.ans2 = res2;
 }
 
 int main(int argc, char *argv[])
@@ -23,7 +85,7 @@ int main(int argc, char *argv[])
     }
 
     //引数が数値なのかのチェック
-    for(int i = 0; i < argc; i++)
+    for(int i = 1; i < argc; i++)
     {
         if(check_arg(argv[i]) == false)
         {
@@ -33,4 +95,25 @@ int main(int argc, char *argv[])
     }
 
     //引数の値を構造体の各メンバに代入
+    t_quad quad = initialize_quad(argv);
+    //a = 0は不正
+    if(quad.a == 0)
+    {
+        cerr << "aが0であるのは不正です。0以外の数値を入力してください。\n";
+        return(1);
+    }
+
+    //二次方程式の計算
+    calc_quad(quad);
+
+    //出力をそれぞれの場合で分ける
+    cout << quad.a << "x\u00B2 + " << quad.b << "x + " << quad.c <<" = 0の解は、\n";
+    if(quad.ans1.img == 0)
+        cout << "x = " << quad.ans1.real << "と、x = " << quad.ans2.real << "です。\n";
+    else
+    {
+        cout << "x = " << quad.ans1.real << " + " << quad.ans1.img << "iと、"
+            << "x = " << quad.ans2.real << " + " << quad.ans2.img << "iです。\n";
+    }
+    return(0);
 }
--- cpj2026_2412747_4/2412747_kadai4-2.cpp
+++ cpj2026_2412747_4/2412747_kadai4-2.cpp
@@ -1,10 +1,105 @@
 #include <iostream>
 using namespace std;
 
+//クラスで使用する数値を定義
+const int EXAM = 100;
+const int SEAT = 150;
+const int SEAT_COVID = 50;
+const int PC = 150;
+const int PROJECTOR = 4;
+
+//教室クラスの宣言
 class ClassRoom
 {
-
+    public:
+        ClassRoom();
+        void PrintMem();
+    protected:
+        int exam_cap;
+        int seat_cap;
+        int seat_cap_covid19;
+        bool have_projector;
+        bool have_screen;
 };
+
+//PC教室クラスの宣言(教室クラスを継承)
+class PCRoom : public ClassRoom
+{
+    public:
+        PCRoom();
+        void PrintPCMem();
+    private:
+        int pc_num;
+        int projector_num;
+        bool have_mic;
+        bool have_wireless_lan;
+};
+
+//教室クラスのコンストラクタの定義
+ClassRoom::ClassRoom()
+{
+    exam_cap = EXAM;
+    seat_cap = SEAT;
+    seat_cap_covid19 = SEAT_COVID;
+    have_projector = true;
+    have_screen = true;
+}
+
+//PC教室クラスについて出力するメンバ関数
+void ClassRoom::PrintMem()
+{
+    cout << "教室には、\n"
+        << "座席が" << seat_cap << "席あり、試験時には" << exam_cap << "席だけ使用できます。"
+        << "また、コロナ禍での座席定員は" << seat_cap_covid19 << "席としていました。\n"
+        << "この教室にはプロジェクターが";
+        if(have_projector == true)
+            cout << "あり、";
+        else
+            cout << "なく、";
+        cout << "スクリーンは";
+        if(have_screen == true)
+            cout << "あります。\n";
+        else
+            cout << "ありません。\n";
+}
+
+//PC教室クラスのコンストラクタの定義
+PCRoom::PCRoom()
+{
+    pc_num = PC;
+    if(have_projector == true)
+        projector_num = PROJECTOR;
+    have_mic = true;
+    have_wireless_lan = true;
+}
+
+void PCRoom::PrintPCMem()
+{
+    cout << "PC教室には、\n"
+        << "座席が" << seat_cap << "席あり、試験時には" << exam_cap << "席だけ使用できます。"
+        << "また、コロナ禍での座席定員は" << seat_cap_covid19 << "席としていました。\n"
+        << "この教室にはプロジェクターが";
+        if(have_projector == true)
+            cout << projector_num << "台あり、";
+        else
+            cout << "なく、";
+        cout << "スクリーンは";
+        if(have_screen == true)
+            cout << "あります。\n";
+        else
+            cout << "ありません。\n";
+        cout << "PCの台数は" << pc_num << "台で、"
+            << "マイクは";
+        if(have_mic == true)
+            cout << "あります。\n";
+        else
+            cout << "ありません。\n";
+        cout << "また、無線LANが整備されて";
+        if(have_wireless_lan == true)
+            cout << "います。\n";
+        else
+            cout << "いません。\n";
+}
 
 int main()
 {
