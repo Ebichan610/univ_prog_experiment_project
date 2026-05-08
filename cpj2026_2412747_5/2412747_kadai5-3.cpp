e14aa19fb10a9f66ebe704d8108d39a0591c8f1b
Ebichan610 <yuya.ebi@gmail.com>
2026-05-08 15:49:38 +0900

    wip

--- cpj2026_2412747_5/2412747_kadai5-3.cpp
+++ cpj2026_2412747_5/2412747_kadai5-3.cpp
@@ -10,22 +10,34 @@ int main(int argc, char* argv[])
         return(1);
     }
 
+    //学習データの処理
     //CSVファイルの読み込み
     cv::Ptr<cv::ml::TrainData> tr1= cv::ml::TrainData::loadFromCSV(argv[1], 0, -2, 0);
     cv::Ptr<cv::ml::TrainData> tr2= cv::ml::TrainData::loadFromCSV(argv[2], 0, -2, 0);
-    cv::Ptr<cv::ml::TrainData> te1= cv::ml::TrainData::loadFromCSV(argv[3], 0, -2, 0);
-    cv::Ptr<cv::ml::TrainData> te2= cv::ml::TrainData::loadFromCSV(argv[4], 0, -2, 0);
-
     //データの切り出し
     cv::Mat data_tr1(200, 2, CV_64FC1);
     cv::Mat data_tr2(200, 2, CV_64FC1);
-    cv::Mat data_te1(50, 2, CV_64FC1);
-    cv::Mat data_te2(50, 2, CV_64FC1);
     data_tr1 = tr1->getSamples();
     data_tr2 = tr2->getSamples();
+    //データのラベル作成
+    
+
+    //識別のためのSVMの構築
+    cv::Ptr<cv::ml::SVM> svm = cv::ml::SVM::create();
+    //SVMパラメータの設定
+    svm->setType(cv::ml::SVM::C_SVC);
+    svm->setKernel(cv::ml::SVM::LINEAR);
+    svm->setTermCriteria(cv::TermCriteria(cv::TermCriteria::MAX_ITER, 100, 1e-6));
+    svm->setC(1.0);
+    
+    //テストデータ
+    //CSVファイルの読み込み
+    cv::Ptr<cv::ml::TrainData> te1= cv::ml::TrainData::loadFromCSV(argv[3], 0, -2, 0);
+    cv::Ptr<cv::ml::TrainData> te2= cv::ml::TrainData::loadFromCSV(argv[4], 0, -2, 0);
+    //データの切り出し
+    cv::Mat data_te1(50, 2, CV_64FC1);
+    cv::Mat data_te2(50, 2, CV_64FC1);
     data_te1 = te1->getSamples();
     data_te2 = te2->getSamples();
-
-    //識別のためのSVMの実装
-    cv::Ptr<cv::ml::SVM> svm = cv::ml::SVM::create();
+    return(0);
 }
